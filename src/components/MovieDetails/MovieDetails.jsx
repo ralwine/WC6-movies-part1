2edb1485a72303fce7acdabcd4120bf002aa5e09
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-08-26 11:58:23 -0500

    completed TODO

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -5,14 +5,30 @@ import { useHistory } from 'react-router-dom';
 export function MovieDetails() {
 
   const history = useHistory();
+  const dispatch = useDispatch();
+  const movies = useSelector(store => store.movies);
 
   const changeScreen = () => {
     console.log("changing screen")
     // Use history.push to navigate to a new route
     history.push('/');
-};
+  };
 
-  return <div>
-    <button className='backToList'onClick={changeScreen}>BACK TO LIST</button>
-  </div>;
+  return (<>
+    <section className="movies">
+      {movies.map(movie => {
+        return (
+          <>
+            {/* <div key={movie.id} >
+              <h3>{movie.title}</h3>
+              <img src={movie.poster} alt={movie.title} onClick={changeScreen} />
+            </div> */}
+          </>
+        );
+      })}
+    </section>
+    <div>
+      <button className='backToList' onClick={changeScreen}>BACK TO LIST</button>
+    </div>
+  </>)
 }
